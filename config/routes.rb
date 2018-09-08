ab35c3e4e7484f1dfc771cfe8c81385b268247da
Tim Fielder <fiel1139@gmail.com>
2018-09-08 13:54:30 -0600

    rearranges routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :conditions, only: [:index, :show]
-
   root to: 'welcome#home'
   get 'welcome/home',  to: 'welcome#home'
+
+  resources :conditions, only: [:index, :show]
 end
