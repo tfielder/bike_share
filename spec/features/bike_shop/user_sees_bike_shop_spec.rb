0f3014b791d14baef36a5d60235ab3243e2ddd51
Tim Fielder <fiel1139@gmail.com>
2018-09-08 16:10:57 -0600

    adds user_sees_bike_shop_spec template.

---
+++ spec/features/bike_shop/user_sees_bike_shop_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'visiting /bike_shop' do
+    it 'shows at least 12 bike accessories for sale' do
+      
+    end
+    it 'shows a button near each item that says add to cart' do
+
+    end
+  end
+end
