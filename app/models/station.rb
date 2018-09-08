e46d5fbe8f639afd95d694b09880abd3af5edc0c
bghalami <benjaminghalami@gmail.com>
2018-09-08 16:18:42 -0600

    added a model test for station, oops

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,6 @@
 class Station < ApplicationRecord
+  validates_presence_of :name, :dock_count, :city, :installation_date
+
   before_save :generate_slug
 
   has_many :trips
@@ -6,7 +8,7 @@ class Station < ApplicationRecord
   def generate_slug
     self.slug = name.parameterize
   end
-  
+
   def to_param
     slug
   end
