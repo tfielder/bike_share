d59f995dd3917ed82987080e00b27cb301498346
Tim Fielder <fiel1139@gmail.com>
2018-09-08 13:48:45 -0600

    adds to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :conditions, only: [:index, :show]
 
-=======
-  root to: 'welcome#home' 
+  root to: 'welcome#home'
   get 'welcome/home',  to: 'welcome#home'
->>>>>>> d1e039ed5c9edeaf441530986ce6302a440ed452
 end
