9d52296685ab42b11fdc0d52a82083a73a3edfcb
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-08 17:35:38 -0600

    add a feature test for viewing the new user page & make test pass

--- spec/users/user_creates_account_spec.rb
+++ spec/users/user_creates_account_spec.rb
@@ -7,15 +7,20 @@ feature 'New user can create an account' do
         visit root_path
 
         click_on "Log In"
+
         expect(current_path).to eq(login_path)
         expect(page).to have_link("Create Account")
       end
-
+    context 'on the login page' do
         context 'I click on Create Account' do
           scenario 'I view a form for creating an account' do
+            
+            visit login_path
+            click_on "Create Account"
             expect(current_path).to eq(new_user_path)
           end
         end
+      end
     end
   end
 end
