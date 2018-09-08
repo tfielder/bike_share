185b5e14eeb41cb5e6fef60f613f6c91812fe144
Tim Fielder <fiel1139@gmail.com>
2018-09-08 10:14:46 -0600

    alignment on Condition model.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,10 @@
 class Condition < ApplicationRecord
-  validates_presence_of :date, :max_temp, :mean_temp, :min_temp, :mean_humidity, :mean_visibility, :mean_wind_speed, :precip
+  validates_presence_of :date
+  validates_presence_of :max_temp
+  validates_presence_of :mean_temp
+  validates_presence_of :min_temp
+  validates_presence_of :mean_humidity
+  validates_presence_of :mean_visibility
+  validates_presence_of :mean_wind_speed
+  validates_presence_of :precip
 end
