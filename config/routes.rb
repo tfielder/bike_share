492a425dd75ea6ada106b01d00dd894be6e45025
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-08 14:18:47 -0600

    add login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'welcome#home' 
+  root to: 'welcome#home'
   get 'welcome/home',  to: 'welcome#home'
+  get '/login', to: 'sessions#new'
 end
