810e03b7e716ce6fbff4019f3d616e56e75f3b47
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-09 00:58:56 -0600

    Takes out save and open page in test.

--- spec/features/trips/user_can_see_trip_details_on_show_page_spec.rb
+++ spec/features/trips/user_can_see_trip_details_on_show_page_spec.rb
@@ -6,7 +6,6 @@ describe 'trip show page' do
     trip_1 = Trip.create(duration: 42, start_date:"09/01/2018", start_station:station_1, end_date:"09/01/2018", end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
 
     visit trip_path(trip_1)
-    save_and_open_page
     expect(page).to have_content(trip_1.duration)
     expect(page).to have_content(trip_1.start_date)
     expect(page).to have_content(trip_1.start_station.name)
