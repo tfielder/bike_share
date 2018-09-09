831f94ad173378e54a438d0b4a7fc331f422e6ae
Tim Fielder <fiel1139@gmail.com>
2018-09-09 09:29:16 -0600

    commenting out flash message for cart.

--- app/controllers/bike_shop_controller.rb
+++ app/controllers/bike_shop_controller.rb
@@ -1,6 +1,6 @@
 class BikeShopController < ApplicationController
   def index
     @accessories = Accessory.all
-    flash[:notice] = "#{@item.title} was added to the cart."
+    # flash[:notice] = "#{@item.title} was added to the cart."
   end
 end
