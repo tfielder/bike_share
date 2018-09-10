245e77e4a5e3146f59774c5773786ebb27b31471
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-10 15:17:03 -0600

    Adds link to trip show page on trip index and test for it.

--- spec/features/trips/user_can_see_trip_details_on_show_page_spec.rb
+++ spec/features/trips/user_can_see_trip_details_on_show_page_spec.rb
@@ -4,8 +4,9 @@ describe 'trip show page' do
     station_1 = Station.create(name:"start station",dock_count: 3, city: "Denver", installation_date:"03/23/2016")
     station_2 = Station.create(name:"end station",dock_count: 3, city: "Denver", installation_date:"03/23/2016")
     trip_1 = Trip.create(duration: 42, start_date:"09/01/2018", start_station:station_1, end_date:"09/01/2018", end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
-
-    visit trip_path(trip_1)
+    visit trips_path
+    click_on trip_1.id
+    expect(current_path).to eq(trip_path(trip_1))
     expect(page).to have_content(trip_1.duration)
     expect(page).to have_content(trip_1.start_date)
     expect(page).to have_content(trip_1.start_station.name)
