8c97de4df9c40a9596fd99632e69801feec2e4e5
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-10 15:26:23 -0600

    add sad path testing for a registered user logging in

--- spec/features/users/user_logs_in_spec.rb
+++ spec/features/users/user_logs_in_spec.rb
@@ -2,28 +2,24 @@ require 'rails_helper'
 
 feature 'Registered user can log in' do
   context 'as a registered user' do
-    context 'when I can click on login' do
-      scenario 'I am logged in' do
-        user = User.create!(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")
+    let!(:user) {User.create!(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")}
 
-        visit root_path
+    before do
+      visit root_path
+      click_on "Log In"
+    end
 
-        click_on "Log In"
+    scenario 'I can log in when I fill out the right info' do
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
 
-        fill_in :email, with: user.email
-        fill_in :password, with: user.password
+      click_on "Submit"
 
-        click_on "Submit"
+      expect(current_path).to eq(dashboard_path)
+      expect(user.name).to eq("Beemo")
+    end
 
-        expect(current_path).to eq(dashboard_path)
-        expect(user.name).to eq("Beemo")
-      end
-      scenario 'I view my profile and user nav bar' do
-        user = User.create!(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")
-
-        visit root_path
-
-        click_on "Log In"
+      scenario 'I view my profile and user nav bar as a logged in user' do
 
         fill_in :email, with: user.email
         fill_in :password, with: user.password
@@ -36,6 +32,15 @@ feature 'Registered user can log in' do
           expect(page).to_not have_content("Log In")
         end
       end
+
+    scenario 'I cannot login when I fill in the wrong info' do
+      fill_in :email, with: "wrong@email.com"
+      fill_in :password, with: user.password
+
+      click_on "Submit"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Incorrect email or password, please try again")
     end
   end
 end
