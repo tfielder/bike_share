a8cde4f136c81c5a09dd6ebac3fd48a1e939f8f0
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-11 17:29:35 -0600

    Creates test file for trips dashboard, adds route.

--- spec/features/dashboards/trips_dashboard_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe'a registered user can visits trips dashboard' do
-  it 'displays the average duration of the ride' do
-    
-  end
-end
---
+++ spec/features/trips/trips_dashboard_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe'a registered user can visits trips dashboard' do
+  it 'displays the average duration of the ride' do
+    station_1 = Station.create(name:"1 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))
+    station_2 = Station.create(name:"2 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))
+
+    trip_1 = Trip.create!(duration: 42, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+    trip_2 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+    trip_3 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+    trip_4 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+
+    visit trips_dashboard_path
+    expect(page).to have_content("Average Duration: #{avg_duration}")
+
+  end
+end
