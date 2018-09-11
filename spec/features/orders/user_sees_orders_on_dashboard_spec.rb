b6f667e11ea49a5329e95e148ca48acfc464e765
bghalami <benjaminghalami@gmail.com>
2018-09-10 18:33:36 -0600

    made working order show page and dashboard

--- spec/features/orders/user_sees_orders_on_dashboard_spec.rb
+++ spec/features/orders/user_sees_orders_on_dashboard_spec.rb
@@ -4,7 +4,7 @@ describe 'as a user' do
   describe "when I visit my dashboard" do
     it "should show me my orders" do
       user = User.create!(name: "Finn", email: "pip@pip.com", password: "123")
-      order = user.orders.create!
+      order = user.orders.create!(status: "completed")
       accessory1 = order.accessories.create!( image: "holder.jpg",
                                  title: "Bonesin",
                                  description: "Some bones",
@@ -15,6 +15,7 @@ describe 'as a user' do
                                  description: "Some bones",
                                  price: 13.35
                                 )
+      order.accessories << accessory2
 
       visit login_path
 
@@ -24,6 +25,11 @@ describe 'as a user' do
       click_on "#{order.id}"
 
       expect(current_path).to eq(order_path(order))
+      expect(page).to have_content(order.status)
+      expect(page).to have_content(order.total_price)
+      expect(page).to have_content(order.updated_at)
+      expect(page).to have_content(order.created_at)
+      save_and_open_page
     end
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,41 @@ describe Order, type: :model do
   describe 'relationships' do
     it {should have_many :order_accessories}
   end
+  describe 'instance methods' do
+    describe '#accessory_count' do
+      it "should return a hash of orders accessories" do
+        user = User.create!(name: "Finn", email: "pip@pip.com", password: "123")
+        order = user.orders.create!
+        accessory1 = order.accessories.create!( image: "holder.jpg",
+                                   title: "Bonesin",
+                                   description: "Some bones",
+                                   price: 13.35
+                                  )
+        accessory2 = order.accessories.create!( image: "holder.jpg",
+                                   title: "Bones",
+                                   description: "Some bones",
+                                   price: 13.35
+                                  )
+        order.accessories << accessory2
+
+        expect(order.accessory_count).to eq({accessory1 => 1, accessory2 => 2})
+      end
+      it "#total_price" do
+        user = User.create!(name: "Finn", email: "pip@pip.com", password: "123")
+        order = user.orders.create!
+        accessory1 = order.accessories.create!( image: "holder.jpg",
+                                   title: "Bonesin",
+                                   description: "Some bones",
+                                   price: 10.00
+                                  )
+        accessory2 = order.accessories.create!( image: "holder.jpg",
+                                   title: "Bones",
+                                   description: "Some bones",
+                                   price: 10.00
+                                  )
+        order.accessories << accessory2
+        expect(order.total_price).to eq(30.00)
+      end
+    end
+  end
 end
