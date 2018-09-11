49fb75820923d2e8f1c9eda701edfa9097b736ab
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-10 21:37:09 -0600

    add before action to require admin to view admin files and add current admin method to application controller

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -1,5 +1,12 @@
 class Admin::StationsController < ApplicationController
+  before_action :require_admin
+
   def index
     @stations = Station.all
   end
+
+  private
+    def require_admin
+      render file: "/public/404" unless current_admin?
+    end 
 end
--- spec/features/stations/admin_views_admin_stations_index_page_spec.rb
+++ spec/features/stations/admin_views_admin_stations_index_page_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
-# As an admin user,
-# When I visit the stations index,
-# I see everything a visitor can see,
-# I see a button next to each station to edit that station,
-# I also see a button next to each station to delete that station.
 
 feature 'Admin views admin stations index page' do
   context 'Admin views edit and delete buttons for each station' do
     context 'as an admin on the stations index page' do
 
+      let!(:station1) { Station.create( name:              "San Jose Diridon Caltrain Station",
+                                        dock_count:        27,
+                                        city:              "San Jose",
+                                        installation_date: "8/6/2013",
+                                      )}
+      let!(:station2) { Station.create( name:              "San Jose Civic Center",
+                                        dock_count:        15,
+                                        city:              "San Jose",
+                                        installation_date: "8/5/2013",
+                                      )}
+      let!(:station3) { Station.create( name:              "Santa Clara",
+                                        dock_count:        11,
+                                        city:              "San Jose",
+                                        installation_date: "8/6/2013",
+                                      )}
+
       before do
         admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-        station1 = Station.create( name:              "San Jose Diridon Caltrain Station",
-                                  dock_count:        27,
-                                  city:              "San Jose",
-                                  installation_date: "8/6/2013",
-                                )
-        station2 = Station.create(name:              "San Jose Civic Center",
-                                  dock_count:        15,
-                                  city:              "San Jose",
-                                  installation_date: "8/5/2013",
-                                )
-        station3 = Station.create(name:              "Santa Clara",
-                                  dock_count:        11,
-                                  city:              "San Jose",
-                                  installation_date: "8/6/2013",
-                                )
+
 
         visit admin_stations_path
       end
@@ -61,6 +58,7 @@ feature 'Admin views admin stations index page' do
       end
     end
   end
+
   context 'as default user' do
     scenario 'I can not see admin stations index' do
       user = User.create!(name: "Ameila Pond", email: "amelia@pond.com", password: "Rory", password_confirmation: "Rory", role: 0)
@@ -68,7 +66,7 @@ feature 'Admin views admin stations index page' do
 
       visit admin_stations_path
 
-      expect(page).to_not have_content("Admin Stations")
+      expect(page).to_not have_content("All Admin Stations")
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
