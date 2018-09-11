2486b37decb92c616215e5a1ab556d9d3d4a44c6
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-11 17:44:33 -0600

    Adds model test for trip average method.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe Trip, type: :model do
     it {should belong_to(:start_station)}
     it {should belong_to(:end_station)}
   end
+  describe 'methods' do
+    it 'should return the average duration' do
+      station_1 = Station.create(name:"1 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))
+      station_2 = Station.create(name:"2 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))
+
+      trip_1 = Trip.create!(duration: 42, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+      trip_2 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+      trip_3 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+      trip_4 = Trip.create(duration: 2, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subsciber", zip_code: 94127 )
+
+      expect(Trip.average_duration).to eq(12)
+    end
+  end
 end
