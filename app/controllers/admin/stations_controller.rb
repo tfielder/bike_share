0d403fcbbadd66aa04a9eeb0c24c14c4c38422aa
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-11 11:13:51 -0600

    add migration to change station installation date from string to date

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -1,7 +1,9 @@
 class Admin::StationsController < Admin::BaseController
-
   def index
     @stations = Station.all
   end
 
+  def show
+    @station = Station.find(slug: params[:slug])
+  end
 end
