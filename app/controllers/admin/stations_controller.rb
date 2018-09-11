71f739f461f3cdf2c9aaca0ed427f9632f22e0af
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-10 21:41:19 -0600

    refactor to put before action in abase controller so it can apply to all admin classes

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -1,12 +1,7 @@
-class Admin::StationsController < ApplicationController
-  before_action :require_admin
+class Admin::StationsController < Admin::BaseController
 
   def index
     @stations = Station.all
   end
 
-  private
-    def require_admin
-      render file: "/public/404" unless current_admin?
-    end 
 end
