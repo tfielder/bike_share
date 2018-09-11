4c6748a82fc2aff0a1b65f3697b34e798e35c4b8
Tim Fielder <fiel1139@gmail.com>
2018-09-11 14:59:57 -0600

    add route for stations dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :stations, only: [:index, :update, :delete]
   end
+
+  get '/stations-dashboard', to: 'stations#dashboard'
 end
