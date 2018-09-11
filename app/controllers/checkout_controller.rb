b298a7e57895553abd4154714bd0750fd1625433
Tim Fielder <fiel1139@gmail.com>
2018-09-11 00:20:15 -0600

    stopping point.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,15 +1,19 @@
 class CheckoutController < ApplicationController
   include ActionView::Helpers::TextHelper
   def create
+    binding.pry
     checkout = Checkout.new(@current_user)
     cart.accessories.each do |accessory, number|
       checkout.accessories.new(accessory: accessory_id, quantity: number)
     end
 
     if checkout.save
-      flash[:notice] = "Successfully submitted your order totaling $#{@cart.sub_total}! You ordered #{@cart.accessories.count} items."
+      total = @cart.sub_total
+      num_items = @cart.accessories.count
       session[:cart] = nil
+      flash[:notice] = "Successfully submitted your order totaling $#{total}! You ordered #{num_items} items."
       redirect_to dashboard_path
+
     else
       flash[:notice] = "Something went wrong, try again later."
       redirect_to bike_shop_path
