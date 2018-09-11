a3467e1fbe0defd11e582845d2741aaf23ee2372
Tim Fielder <fiel1139@gmail.com>
2018-09-11 17:07:09 -0600

    finalizes user_sees_stations_dashboard_spec and stations controller.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -15,8 +15,10 @@ class StationsController < ApplicationController
     ordered_dock_count = stations.order(dock_count: :DESC)
     @most_bikes = ordered_dock_count.first.dock_count
     @least_bikes = ordered_dock_count.last.dock_count
-    @station_most_bikes = ordered_dock_count.first.name
-    @station_least_bikes = ordered_dock_count.last.name
+    station_most_bikes_names = ordered_dock_count.where("dock_count = ?", "#{@most_bikes}")
+    @station_most_bikes_names = station_most_bikes_names.map {|station| station.name}
+    station_least_bikes_names = ordered_dock_count.where("dock_count = ?", "#{@least_bikes}")
+    @station_least_bikes_names = station_least_bikes_names.map {|station| station.name}
     ordered_installation_date = stations.order(installation_date: :DESC)
     @newest_station = ordered_installation_date.first.name
     @oldest_station = ordered_installation_date.last.name
--- spec/features/stations/user_sees_stations_dashboard_spec.rb
+++ spec/features/stations/user_sees_stations_dashboard_spec.rb
@@ -29,8 +29,10 @@ describe 'as a registered user' do
       ordered_dock_count = stations.order(dock_count: :DESC)
       @most_bikes = ordered_dock_count.first.dock_count
       @least_bikes = ordered_dock_count.last.dock_count
-      @station_most_bikes = ordered_dock_count.first.name
-      @station_least_bikes = ordered_dock_count.last.name
+      station_most_bikes_names = ordered_dock_count.where("dock_count = ?", "#{@most_bikes}")
+      @station_most_bikes_names = station_most_bikes_names.map {|station| station.name}
+      station_least_bikes_names = ordered_dock_count.where("dock_count = ?", "#{@least_bikes}")
+      @station_least_bikes_names = station_least_bikes_names.map {|station| station.name}
       ordered_installation_date = stations.order(installation_date: :DESC)
       @newest_station = ordered_installation_date.first.name
       @oldest_station = ordered_installation_date.last.name
@@ -45,10 +47,10 @@ describe 'as a registered user' do
       expect(page).to have_content("Average Bikes Available Per Station: #{@average}")
     end
     it "shows Most bikes available at a station (based on docks) and where most bikes available" do
-      expect(page).to have_content("Most Bikes Available: #{@most_bikes} at Station: #{@station_most_bikes}")
+      expect(page).to have_content("#{@most_bikes} at Station: #{@station_most_bikes_names.first}")
     end
     it "shows the Station(s) where the fewest bikes are available (based on docks)" do
-      expect(page).to have_content("Fewest Bikes Available: #{@least_bikes} at Station: #{@station_least_bikes}")
+      expect(page).to have_content("#{@least_bikes} at Station: #{@station_least_bikes_names.first}")
     end
     it "shows Most recently installed station" do
       expect(page).to have_content("Newest Installed Station: #{@newest_station}")
