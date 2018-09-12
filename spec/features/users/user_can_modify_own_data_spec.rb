ee049eeb9ed058c926992778d916b03d51694ce9
Tim Fielder <fiel1139@gmail.com>
2018-09-11 23:50:29 -0600

    test for editing account view and button.

--- spec/features/users/user_can_modify_own_data_spec.rb
+++ spec/features/users/user_can_modify_own_data_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 describe 'As a registered user' do
   before(:each) do
-    let!(:user1) {User.create!(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")}
-    let!(:user2) {User.create!(name: "Zebra", email: "zebra@email.com", password: "test", password_confirmation: "test")}
+    user1 = User.create(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")
+    user2 = User.create(name: "Zebra", email: "zebra@email.com", password: "test", password_confirmation: "test")
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:user2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user2)
+  end
+  describe 'from user dashboard, can access edit_account_path' do
+    visit dashboard_path
 
-    visit login_path
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_button("Edit Account")
+
+    click_on "Edit Account"
+
+    expect(current_path).to eq(user_edit_path)
   end
   describe 'from the edit_account' do
     it 'allows user to modify account data' do
@@ -20,8 +28,8 @@ describe 'As a registered user' do
 end
 describe 'as an admin' do
   before(:each) do
-    admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
-    admin2 = User.create!(name: "Dr.What", email: "theotherdoctor@tardis.com", password: "red", password_confirmation: "wrong", role: 1)
+    admin = User.create(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
+    admin2 = User.create(name: "Dr.What", email: "theotherdoctor@tardis.com", password: "red", password_confirmation: "wrong", role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
