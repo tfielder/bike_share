4c3c2f1ee02d06e494902f21e69e120bd2efdc65
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-11 23:13:42 -0600

    add private set station method

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -1,11 +1,11 @@
 class Admin::StationsController < Admin::BaseController
-
+before_action :set_station, only: [:destroy, :edit, :show, :update]
   def index
     @stations = Station.all
   end
 
   def show
-    @station = Station.find_by(slug: params[:slug])
+
   end
 
   def new
@@ -19,25 +19,26 @@ class Admin::StationsController < Admin::BaseController
   end
 
   def edit
-    @station = Station.find_by(slug: params[:slug])
     @admin = current_user.role
   end
 
   def update
-    station = Station.find_by(slug:params[:slug])
-    station.update(station_params)
+    @station.update(station_params)
     flash[:notice] = "Successfully updated!"
-    redirect_to admin_station_path(station)
+    redirect_to admin_station_path(@station)
   end
 
   def destroy
-    station = Station.find_by(slug:params[:slug])
-    station.destroy
+    @station.destroy
     redirect_to admin_stations_path, notice: "Successfully deleted."
   end
 
   private
   def station_params
     params.require(:station).permit(:name, :dock_count, :city, :installation_date)
+  end
+
+  def set_station
+    @station = Station.find_by(slug: params[:slug])
   end
 end
