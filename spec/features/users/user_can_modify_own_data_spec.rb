ff07e79e97a752c4f456bde0fe5e43f1ab1eacc2
Tim Fielder <fiel1139@gmail.com>
2018-09-11 23:41:53 -0600

    template for user_can_modify_own_data_spec.

---
+++ spec/features/users/user_can_modify_own_data_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'As a registered user' do
+  before(:each) do
+    let!(:user1) {User.create!(name: "Beemo", email: "bmo@email.com", password: "pass", password_confirmation: "pass")}
+    let!(:user2) {User.create!(name: "Zebra", email: "zebra@email.com", password: "test", password_confirmation: "test")}
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:user2)
+
+    visit login_path
+  end
+  describe 'from the edit_account' do
+    it 'allows user to modify account data' do
+
+    end
+    it 'does not allow user to modify other user account data' do
+
+    end
+  end
+end
+describe 'as an admin' do
+  before(:each) do
+    admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
+    admin2 = User.create!(name: "Dr.What", email: "theotherdoctor@tardis.com", password: "red", password_confirmation: "wrong", role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit login_path
+  end
+  describe 'from the edit_account' do
+    it 'allows user to modify account data' do
+
+    end
+    it 'does not allow user to modify other user account data' do
+
+    end
+  end
+end
