23d75c626eb66031bd90bfa454b4cf3baf518545
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-12 00:22:58 -0600

    add feature test for admin views trips show page

---
+++ spec/features/admins/trips/admin_views_admin_trips_show_page_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+feature 'Admin views admin trip show page' do
+  context 'Admin views edit and delete buttons for a trip' do
+    context 'as an admin on the trip show page' do
+
+        let!(:station){Station.create(name:"1 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))}
+        let!(:trip) {Trip.create!(duration: 42, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station:station_2,bike_id: 2, subscription_type:"Subscriber", zip_code: 94127 )}
+        let!(:station_2){Station.create(name:"2 station",dock_count: 3, city: "Denver", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))}
+
+      before do
+        admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit admin_trip_path
+      end
+
+        scenario 'I am on the admin trip show page' do
+          expect(current_path).to eq(admin_trip_path(trip))
+          expect(page).to have_content("Admin Trip")
+        end
+
+        scenario 'I view an edit button for a trip' do
+          expect(page).to have_link("Edit")
+        end
+
+        scenario 'I view a delete button for a trip' do
+          expect(page).to have_link("Delete")
+        end
+
+        scenario 'I view everything else a user sees' do
+          expect(page).to have_content(trip.duration)
+          expect(page).to have_content(station_1.name)
+          expect(page).to have_content(station_2.name)
+          expect(page).to have_content(trip.start_date)
+          expect(page).to have_content(trip.end_date)
+          expect(page).to have_content(trip.bike_id)
+          expect(page).to have_content(trip.subscription_type)
+        end
+    end
+  end
+
+  context 'as default user' do
+    scenario 'I can not see admin trip show' do
+      user = User.create!(name: "Ameila Pond", email: "amelia@pond.com", password: "Rory", password_confirmation: "Rory", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_trip_path
+
+      expect(page).to_not have_content("Admin Trip")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+  context 'as visitor' do
+    scenario 'I can not see admin trip show' do
+
+      visit admin_trip_path
+
+      expect(page).to_not have_content("Admin Trip")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+end
