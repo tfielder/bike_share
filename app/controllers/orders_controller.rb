79f2a67c91bbf783ca23e89c31778bdbb9c129a1
Tim Fielder <fiel1139@gmail.com>
2018-09-13 14:47:12 -0600

    conditions test as user.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    #
     if current_admin? || (current_user && @order.user == current_user)
       @accessories = @order.accessory_count
       @total_price = @order.total_price
