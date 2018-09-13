bae2446960712d74e9873f5b7bcd033ce0c84a80
Tim Fielder <fiel1139@gmail.com>
2018-09-12 22:53:50 -0600

    formatting orders.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,8 +1,10 @@
 class CheckoutController < ApplicationController
   include ActionView::Helpers::TextHelper
   def create
-    current_user.orders.create(status: "pending")
-
+      order = current_user.orders.create(status: "ordered")
+      @cart.contents.each do |key, value|
+        order.order_accessories.create(order_id: order.id, accessory_id: key)
+      end
     if current_user.orders
       total = '%.2f' % @cart.cart_total
       num_items = @cart.accessories.count
@@ -14,4 +16,5 @@ class CheckoutController < ApplicationController
       redirect_to bike_shop_path
     end
   end
+
 end
