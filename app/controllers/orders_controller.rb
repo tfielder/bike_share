b1a8c6b29618e3cf2569e5a22654112752246611
Tim Fielder <fiel1139@gmail.com>
2018-09-13 13:44:02 -0600

    user can only see own orders.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    if current_user.orders.include?(@order)
+    if current_user && current_user.orders.include?(@order)
       @accessories = @order.accessory_count
       @total_price = @order.total_price
     else
