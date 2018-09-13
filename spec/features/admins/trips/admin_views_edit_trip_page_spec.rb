aa049ea08ce2ca30af51e80c77626166cc173d58
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-12 22:46:08 -0600

    add test for admin edit and update trip functionality

---
+++ spec/features/admins/trips/admin_views_edit_trip_page_spec.rb
@@ -0,0 +1,101 @@
+require 'rails_helper'
+
+feature 'Admin edits trip' do
+  let!(:station_1){Station.create(name:"Heimerdinger station",dock_count: 3, city: "Galway", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))}
+  let!(:trip) {Trip.create!(duration: 42, start_date: ("09/01/2018"), start_station:station_1, end_date: ("09/01/2018"), end_station: station_2, bike_id: 2, subscription_type:"Noob", zip_code: 94127 )}
+  let!(:station_2){Station.create(name:"Poppy station",dock_count: 3, city: "Toronto", installation_date: Date.strptime("03/23/2016", '%m/%d/%Y'))}
+
+  context 'as an admin on the trips index page' do
+    before do
+      admin = User.create!(name: "Leela", email: "leela@futurama.com", password: "purple", password_confirmation: "purple", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit admin_trips_path
+    end
+
+    context 'when I click on Edit' do
+      scenario 'it takes me to an edit form' do
+        click_on "Edit"
+
+        expect(current_path).to eq(edit_admin_trip_path(trip))
+        expect(page).to have_content("Edit trip: ##{trip.id}")
+        expect(page).to have_content("#{trip.duration}")
+        expect(page).to have_content(trip.start_date)
+        expect(page).to have_content(trip.end_date)
+        expect(page).to have_content("#{trip.bike_id}")
+        expect(page).to have_content(trip.subscription_type)
+        expect(page).to have_content(station_1.name)
+        expect(page).to have_content(station_2.name)
+      end
+
+      context 'when I update the form and click submit' do
+        scenario 'it updates a station' do
+          click_on "Edit"
+          fill_in :trip_subscription_type, with: "The best subscriber"
+          click_on "Submit Changes"
+          trip.reload
+
+          expect(current_path).to eq(trip_station_path(trip))
+          expect(trip).to have_content("Successfully updated!")
+        end
+      end
+    end
+  end
+  context 'as an admin on the trips show page' do
+    before do
+      admin = User.create!(name: "Leela", email: "leela@futurama.com", password: "purple", password_confirmation: "purple", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit admin_trip_path(trip)
+    end
+
+    context 'when I click on Edit' do
+      scenario 'it takes me to an edit form' do
+        click_on "Edit"
+
+        expect(current_path).to eq(edit_admin_trip_path(trip))
+        expect(page).to have_content("Edit trip: ##{trip.id}")
+        expect(page).to have_content("#{trip.duration}")
+        expect(page).to have_content(trip.start_date)
+        expect(page).to have_content(trip.end_date)
+        expect(page).to have_content("#{trip.bike_id}")
+        expect(page).to have_content(trip.subscription_type)
+        expect(page).to have_content(station_1.name)
+        expect(page).to have_content(station_2.name)
+      end
+
+      context 'when I update the form and click submit' do
+        scenario 'it updates a station' do
+          click_on "Edit"
+          fill_in :trip_subscription_type, with: "The best subscriber"
+          click_on "Submit Changes"
+          trip.reload
+
+          expect(current_path).to eq(trip_station_path(trip))
+          expect(trip).to have_content("Successfully updated!")
+        end
+      end
+    end
+  end
+  context 'as default user' do
+    before do
+      user = User.create!(name: "Bender", email: "bender@futurama.com", password: "money", password_confirmation: "money", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit edit_admin_trip_path(trip)
+    end
+
+    scenario 'I cannot see the edit station page' do
+      expect(page).to_not have_content("Edit trip: ##{trip.id}")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+  context 'as a visitor' do
+    before do
+      visit edit_admin_trip_path(trip)
+    end
+
+    scenario 'I cannot see the edit station page' do
+      expect(page).to_not have_content("Edit trip: ##{trip.id}")
+      expext(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
+end
