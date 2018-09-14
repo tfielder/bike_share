97c189ed6fb8f10de9fcb243aa8b26f972c96b43
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-13 19:09:00 -0600

    refactor--make method to find trip private

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,7 +1,7 @@
 class Admin::TripsController < Admin::BaseController
+before_action :set_trip, only: [:destroy, :edit, :update]
 
   def destroy
-    @trip = Trip.find(params[:id])
     @trip.destroy
     flash[:notice] = "Successfully deleted."
     redirect_to trips_path
@@ -9,12 +9,10 @@ class Admin::TripsController < Admin::BaseController
 
   def edit
     @admin = current_user.role
-    @trip = Trip.find(params[:id])
     @station = Station.find_by(params[@trip[:start_station_id]])
   end
 
   def update
-    @trip = Trip.find(params[:id])
     @trip.update(trip_params)
     flash[:notice] = "Successfully updated!"
     redirect_to trip_path(@trip)
@@ -23,5 +21,9 @@ class Admin::TripsController < Admin::BaseController
   private
   def trip_params
     params.require(:trip).permit(:duration, :start_station_id, :start_date, :end_station_id, :end_station, :end_date, :bike_id, :subscription_type, :zip_code)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
   end
 end
