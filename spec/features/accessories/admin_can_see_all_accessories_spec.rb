a283f591cf857075142bb33f819a58606660ae33
Tim Fielder <fiel1139@gmail.com>
2018-09-15 16:42:20 -0600

    test for editing an accessory.

--- spec/features/accessories/admin_can_see_all_accessories_spec.rb
+++ spec/features/accessories/admin_can_see_all_accessories_spec.rb
@@ -6,15 +6,6 @@ describe 'as an admin' do
       @item_1 = Accessory.create(title: "happy1", image: "image1", description: "sogood1", price: 10.01)
       @item_2 = Accessory.create(title: "happy2", image: "image2", description: "sogood2", price: 10.02)
       @item_3 = Accessory.create(title: "happy3", image: "image3", description: "sogood3", price: 10.03)
-      @item_4 = Accessory.create(title: "happy4", image: "image4", description: "sogood4", price: 10.04)
-      @item_5 = Accessory.create(title: "happy5", image: "image5", description: "sogood5", price: 10.05)
-      @item_6 = Accessory.create(title: "happy6", image: "image6", description: "sogood6", price: 10.06)
-      @item_7 = Accessory.create(title: "happy7", image: "image7", description: "sogood7", price: 10.07)
-      @item_8 = Accessory.create(title: "happy8", image: "image8", description: "sogood8", price: 10.08)
-      @item_9 = Accessory.create(title: "happy9", image: "image9", description: "sogood9", price: 10.09)
-      @item_10 = Accessory.create(title: "happy10", image: "image10", description: "sogood10", price: 10.10)
-      @item_11 = Accessory.create(title: "happy11", image: "image11", description: "sogood11", price: 10.20)
-      @item_12 = Accessory.create(title: "happy12", image: "image12", description: "sogood12", price: 10.30)
 
       @admin = User.create!(name: "Dr.Who", email: "thedoctor@tardis.com", password: "blue", password_confirmation: "blue", role: 1)
 
@@ -54,6 +45,20 @@ describe 'as an admin' do
       expect(page).to have_content("Edit")
     end
     it 'allows admin to edit an accessory' do
+      visit admin_bike_shop_path
+
+      expect(page).to have_content(@item_1.title)
+      expect(page).to have_content(@item_1.description)
+      expect(page).to have_content(@item_1.price)
+      expect(page).to have_css("img[src*='#{@item_1.image}']")
+      expect(page).to have_content(@item_2.title)
+      expect(page).to have_content(@item_2.description)
+      expect(page).to have_content(@item_2.price)
+      expect(page).to have_css("img[src*='#{@item_2.image}']")
+
+      click_on ("Edit").first
+
+      expect(current_path).to eq(edit_admin_accessory_path)
 
     end
     it 'allows an admin to retire an accessory' do
