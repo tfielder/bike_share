d90ec21944964ecfa51faa4a8e151959ca6ef15b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-15 13:36:55 -0600

    Adds accessory

--- spec/features/admins/orders/admin_sees_individual_order_page_spec.rb
+++ spec/features/admins/orders/admin_sees_individual_order_page_spec.rb
@@ -5,10 +5,17 @@ describe 'Admin views an individual order they see order detail w/ date and addr
     @admin = User.create!(name: "Finn", email: "pip@pip.com", password: "123", role: 1)
     @user1 = User.create!(name: "sue", email: "you@you.com", password: "123")
     @user2 = User.create!(name: "You", email: "p@p.com", password: "123")
-
+    @accessory = Accessory.create!( image: "https://i.kym-cdn.com/photos/images/masonry/000/138/153/voldemort-120-thumb-120x120-34926.jpg",
+                       title: "Voldemort",
+                       description: "Noseless Racist Wizard",
+                       price: 48.50
+                    )
     @order = @user1.orders.create!(status: "pending")
+    @order.accessories << @accessory
+
   end
   scenario 'I visit the order show page as an admin'do
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
     visit order_path(@order)
