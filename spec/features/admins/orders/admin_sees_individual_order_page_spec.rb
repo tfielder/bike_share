e2806a37071423a5d923b5c29d97aadcc3d5b289
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-15 15:58:19 -0600

    Creates tables

--- spec/features/admins/orders/admin_sees_individual_order_page_spec.rb
+++ spec/features/admins/orders/admin_sees_individual_order_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Admin views an individual order they see order detail w/ date and address' do
   before(:each) do
     @admin = User.create!(name: "Finn", email: "pip@pip.com", password: "123", role: 1)
-    @user1 = User.create!(name: "sue", email: "you@you.com", password: "123")
+    @user1 = User.create!(name: "Sue Quinn", email: "you@you.com", password: "123")
     @user2 = User.create!(name: "You", email: "p@p.com", password: "123")
     @accessory = Accessory.create!( image: "https://i.kym-cdn.com/photos/images/masonry/000/138/153/voldemort-120-thumb-120x120-34926.jpg",
                        title: "Voldemort",
@@ -19,7 +19,6 @@ describe 'Admin views an individual order they see order detail w/ date and addr
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
     visit order_path(@order)
-    save_and_open_page
 
     expect(page).to have_content(Order.last.created_at)
     expect(page).to have_content(@user1.name)
