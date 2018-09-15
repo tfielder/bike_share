78c52a3567cf3adc9cf611a52beb17fe8eb962ee
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-15 12:31:25 -0600

    Creates test for admin viewing order.

---
+++ spec/features/admins/orders/admin_sees_individual_order_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Admin views an individual order they see order detail w/ date and address' do
+  before(:each) do
+    @admin = User.create!(name: "Finn", email: "pip@pip.com", password: "123", role: 1)
+    @user1 = User.create!(name: "sue", email: "you@you.com", password: "123")
+    @user2 = User.create!(name: "You", email: "p@p.com", password: "123")
+
+    @order = @user1.orders.create!(status: "completed")
+  end
+  scenario 'I visit the order show page as an admin'do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit order_path(@order)
+
+    expect(page).to have_content(Order.last.created_at)
+    expect(page).to have_content(@user1.name)
+
+  end
+end
