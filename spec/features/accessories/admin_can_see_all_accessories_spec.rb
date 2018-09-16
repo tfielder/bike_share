ab971210df23408431f8506086f335e602b3d070
Tim Fielder <fiel1139@gmail.com>
2018-09-16 14:34:58 -0600

    adds admin bike path visit to test for admin seeing accessories.

--- spec/features/accessories/admin_can_see_all_accessories_spec.rb
+++ spec/features/accessories/admin_can_see_all_accessories_spec.rb
@@ -26,18 +26,19 @@ describe 'as an admin' do
 
     end
     it 'shows a table with all accessories' do
-      save_and_open_page
-      expect(page).to have_content(@item_1.title)
-      expect(page).to have_content(@item_1.description)
-      expect(page).to have_content(@item_1.price)
+      visit admin_bike_shop_path
+
+      expect(page).to have_content("#{@item_1.title}")
+      expect(page).to have_content("#{@item_1.description}")
+      expect(page).to have_content("#{@item_1.price}")
       expect(page).to have_css("img[src*='#{@item_1.image}']")
-      expect(page).to have_content(@item_2.title)
-      expect(page).to have_content(@item_2.description)
-      expect(page).to have_content(@item_2.price)
+      expect(page).to have_content("#{@item_2.title}")
+      expect(page).to have_content("#{@item_2.description}")
+      expect(page).to have_content("#{@item_2.price}")
       expect(page).to have_css("img[src*='#{@item_2.image}']")
-      expect(page).to have_content(@item_3.title)
-      expect(page).to have_content(@item_3.description)
-      expect(page).to have_content(@item_3.price)
+      expect(page).to have_content("#{@item_3.title}")
+      expect(page).to have_content("#{@item_3.description}")
+      expect(page).to have_content("#{@item_3.price}")
       expect(page).to have_css("img[src*='#{@item_3.image}']")
 
       expect(page).to have_content("Retire")
