4230c1a02dec1331a1e7fc1a1154dc87bfedf27f
Tim Fielder <fiel1139@gmail.com>
2018-09-17 11:47:59 -0600

    adds routes.

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
     resources :trips, only: [:update, :destroy, :new, :create, :edit]
     resources :conditions, only: [:update, :destroy, :new, :create, :edit]
     resource :dashboard, only: [:show]
-    resources :accessories, only: [:edit, :update, :toggle], param: :slug do
+    resources :accessories, only: [:edit, :update, :toggle, :new, :create], param: :slug do
       patch :update_accessory, on: :member
     end
   end
