8a2106d884aa987e77a35e6415b0b25d0da73beb
bghalami <benjaminghalami@gmail.com>
2018-09-18 18:50:39 -0600

    fixed bug where trips on trips dashboard weren't grouping by month

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -31,7 +31,7 @@ class Trip < ApplicationRecord
   end
 
   def self.monthly_breakdown
-    Trip.select("date_trunc('month', start_date) AS month,count(start_date)").group("trips.start_date")
+    Trip.select("date_trunc('month', start_date) AS month, count(start_date)").group("date_trunc('month', start_date)").order("date_trunc('month', start_date)")
   end
 
   def self.user_subscription_breakdown
